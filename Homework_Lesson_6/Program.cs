743ee3710900fb2d7f30d0e4440bf61303d03af9
Mad <mad.nataniel@gmail.com>
2022-04-21 22:04:19 +0300

    Выполнил ДЗ 6

--- Homework_Lesson_6/Program.cs
+++ Homework_Lesson_6/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
 
 namespace Homework_Lesson_6
 {
@@ -6,7 +8,35 @@ namespace Homework_Lesson_6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var processList = Process.GetProcesses(); // получаем массив процессов
+            foreach (var proc in processList)
+            {
+                Console.WriteLine($"ID: {proc.Id} \t Name: {proc.ProcessName} ");
+            }
+
+            Console.Write("Введите Имя или ID процесса,который хотите завершить: ");
+            string nameproc = Console.ReadLine();
+
+            try // проверка по id
+            {
+
+                processList.First(i => i.Id.ToString() == nameproc).Kill();
+                Console.WriteLine($"Поцесс {nameproc} завершен..");
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine($"ID процесса: {nameproc} не найден!");
+            }
+
+            try // проверка по имени
+            {
+                processList.First(s => s.ProcessName.ToLower() == nameproc.ToLower()).Kill();
+                Console.WriteLine($"Процесс {nameproc} завершен..");
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine($"Имя процесса: {nameproc} не найдено!");
+            }
         }
     }
 }
